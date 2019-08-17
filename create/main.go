4b29dd3611891e1b3ec8ea35542d7495e3db969a
jpart-r <jpartdev@gmail.com>
2019-08-18 01:36:03 +0800

    Infrastructure updates

--- create/main.go
+++ create/main.go
@@ -2,18 +2,13 @@ package main
 
 import "github.com/aws/aws-lambda-go/lambda"
 
-type Message struct {
-	CreatedAt string
-	Message   string
-}
-
 type response struct {
 	Message string `json:"message"`
 }
 
 func handler() (response, error) {
 	return response{
-		Message: "Okay so your other function also executed successfully!",
+		Message: "test message",
 	}, nil
 }
 
