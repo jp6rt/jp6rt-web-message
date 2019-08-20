09383540770dc8e2cfc93a231faccc85219f1410
jpart-r <jpartdev@gmail.com>
2019-08-20 23:22:03 +0800

    Dynamo DB put item

--- handlers/create.go
+++ handlers/create.go
@@ -2,31 +2,82 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"os"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	uuid "github.com/satori/go.uuid"
 )
 
-// Response ..
-type Response struct {
-	Message string `json:"message"`
-	Code    int    `json:"code"`
+// Message ..
+type Message struct {
+	ID        string `json:"id"`
+	Message   string `json:"message"`
+	CreatedAt int64  `json:"createdAt"`
 }
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	x := Response{
-		Message: "ok",
-		Code:    200,
-	}
-	response, err := json.Marshal(x)
+var db *dynamodb.DynamoDB
+
+func init() {
+	fmt.Println("init")
+	region := os.Getenv("AWS_REGION")
+	session, err := session.NewSession(&aws.Config{
+		Region: &region,
+	})
 
 	if err != nil {
-		return events.APIGatewayProxyResponse{Body: string(response), StatusCode: 200}, nil
+		fmt.Println(fmt.Sprintf("Failed to connect to AWS: %s", err.Error()))
+		os.Exit(1)
 	}
 
-	return events.APIGatewayProxyResponse{Body: string(response), StatusCode: 500}, nil
+	db = dynamodb.New(session)
+	fmt.Println("db initiated")
+}
+
+// Handler ..
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	fmt.Println("Handler")
+
+	uuid := uuid.Must(uuid.NewV4()).String()
+	tableName := aws.String("dev-jp6rt-web-messages")
+	now := time.Now()
+
+	message := &Message{
+		ID:        uuid,
+		CreatedAt: now.Unix(),
+	}
+
+	json.Unmarshal([]byte(request.Body), message)
+
+	item, _ := dynamodbattribute.MarshalMap(message)
+	input := &dynamodb.PutItemInput{
+		Item:      item,
+		TableName: tableName,
+	}
+
+	_, err := db.PutItem(input)
+
+	if err != nil {
+		fmt.Println("Error putting item")
+		return events.APIGatewayProxyResponse{
+			Body:       err.Error(),
+			StatusCode: 500,
+		}, nil
+	}
+
+	body, _ := json.Marshal(message)
+	return events.APIGatewayProxyResponse{
+		Body:       string(body),
+		StatusCode: 200,
+	}, nil
 }
 
 func main() {
-	lambda.Start(handler)
+	lambda.Start(Handler)
 }
