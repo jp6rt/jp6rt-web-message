20e9d19d4bac2715cb59830e76168d8cb2e3ee61
jp6rt <jp01r7@gmail.com>
2019-09-27 13:55:01 +0800

    Fix unary operator expected

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 set -e
 aws cloudformation deploy --stack-name ${STACK_NAME} --template-file ./infra/resources-template.yml || EXIT_CODE=$?
 
-if [ $EXIT_CODE -eq 255 ]
+if [ "$EXIT_CODE" -eq 255 ]
 then 
   echo "No updates needed to deploy"
   exit 0
